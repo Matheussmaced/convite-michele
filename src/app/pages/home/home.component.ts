a01ab91a71368f82b8dab68273a9bf891561793e
Matheussmaced <mateusbyakuya3@gmail.com>
2024-12-11 09:49:12 -0300

    fix: safari

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,17 +7,28 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewChecked  {
   @ViewChild('videoElement') videoElement!: ElementRef<HTMLVideoElement>;
+  @ViewChild('interactiveLayer') interactiveLayer!: ElementRef<HTMLDivElement>;
+
   showPlayButton = true;
+
+  ngAfterViewChecked(): void {
+    // Agora você pode acessar o ViewChild depois que a visualização foi inicializada
+    if (this.videoElement && this.interactiveLayer) {
+      // Apenas exibe uma mensagem caso os elementos existam
+      console.log('Video element and interactive layer are available');
+    }
+  }
 
   playVideo(): void {
     const video = this.videoElement.nativeElement;
+    const layer = this.interactiveLayer.nativeElement;
 
     video.play()
       .then(() => {
-        console.log('Vídeo reproduzido com sucesso');
-        this.showPlayButton = false
+        this.showPlayButton = false; // Esconde o botão de play
+        layer.style.pointerEvents = 'all'; // Habilita a interação nos links
       })
       .catch(err => {
         console.error('Erro ao reproduzir o vídeo:', err);
