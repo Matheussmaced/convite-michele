662d4cd67a31bd3643d85b46d41c870601d91302
Matheussmaced <mateusbyakuya3@gmail.com>
2024-12-11 08:48:41 -0300

    feat: creating links

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: false,
-  
+
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  @ViewChild('videoElement') videoElement!: ElementRef<HTMLVideoElement>;
+  showPlayButton = true;
 
+  playVideo(): void {
+    const video = this.videoElement.nativeElement;
+
+    video.muted = false;
+    video.play().then(() => {
+      this.showPlayButton = false;
+    }).catch(err => {
+      console.error('Erro ao reproduzir o vídeo:', err);
+    });
+  }
 }
